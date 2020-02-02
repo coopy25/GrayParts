f1069d0c954ca44a1935f836788ed39eab9018b3
Martin Ramke <martin.ramke@ramtrix.de>
2020-02-02 11:15:43 +0100

    added comments

--- GrayParts.control.js
+++ GrayParts.control.js
@@ -5,12 +5,17 @@
 loadAPI (2);
 host.defineController ("Tools", "Gray Parts", "1.0", "656bca90-4598-11ea-aaef-0800200c9a66", "Nocteb");
 
+
+// This constants define the controllers area of action (set to something big for now)
 var NUM_TRACKS=64
 var NUM_SCENES=64
 
+// this array tracks each clips playing state. The array contains a dict for each track. 
+// The dictionary contains the clip index and it's playing state
 var tracks = []
 
-
+// Helper function to store the track index and the slot bank with the function
+// see https://stackoverflow.com/questions/13813463/how-to-avoid-access-mutable-variable-from-closure
 function createTrackCallback(slotBank, trackIndex, f)
 {
     return function(index, playing)
@@ -19,9 +24,10 @@ function createTrackCallback(slotBank, trackIndex, f)
     };
 }
 
-
+// The controllers init function
 function init ()
 {
+    // create a track and scene info to work with change the constants above as required
     var trackBank = host.createMainTrackBank(NUM_TRACKS,0,NUM_SCENES);
     for(var i=0; i < NUM_TRACKS; ++i)
     {
@@ -29,18 +35,25 @@ function init ()
         if(!track) {
             continue;
         }
+        // add an observer to each tracks clipLauncherSlotBank
         var slotBank = track.clipLauncherSlotBank()
         if(slotBank) {
             slotBank.addIsPlayingObserver(
+                // call the function wrapper above 
                 createTrackCallback(slotBank, i,
+                    // the function that gets executed when a clips playing state changes
                     function(slotBank, trackIndex, index, playing)
                     {
+                        // extend the array tracking playing state
                         if(!tracks[trackIndex]) {
                             tracks[trackIndex] = {}
                         }
+                        // if a clip is not playing and was playing before
                         if(!playing && tracks[trackIndex][index] == true) {
+                            // set the color
                             slotBank.getItemAt(index).color().set(0.5,0.5,0.5)
                         }
+                        // record the clips playing state
                         tracks[trackIndex][index] = playing;
                     }
                 )
