3896352443a0a62ae9e8f185afd39ed748363f52
Martin Ramke <martin.ramke@ramtrix.de>
2020-02-04 18:18:16 +0100

    - Increased track and scene window - Clips only get colored when transport is playing

--- GrayParts.control.js
+++ GrayParts.control.js
@@ -3,12 +3,12 @@
 // Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
 
 loadAPI (2);
-host.defineController ("Tools", "Gray Parts", "1.0", "656bca90-4598-11ea-aaef-0800200c9a66", "Nocteb");
+host.defineController ("Tools", "Gray Parts", "1.1", "656bca90-4598-11ea-aaef-0800200c9a66", "Nocteb");
 
 
 // This constants define the controllers area of action (set to something big for now)
-var NUM_TRACKS=64
-var NUM_SCENES=64
+var NUM_TRACKS=256
+var NUM_SCENES=256
 
 // this array tracks each clips playing state. The array contains a dict for each track. 
 // The dictionary contains the clip index and it's playing state
@@ -16,11 +16,11 @@ var tracks = []
 
 // Helper function to store the track index and the slot bank with the function
 // see https://stackoverflow.com/questions/13813463/how-to-avoid-access-mutable-variable-from-closure
-function createTrackCallback(slotBank, trackIndex, f)
+function createTrackCallback(transport, slotBank, trackIndex, f)
 {
     return function(index, playing)
     {
-        f(slotBank, trackIndex, index, playing);
+        f(transport, slotBank, trackIndex, index, playing);
     };
 }
 
@@ -29,6 +29,12 @@ function init ()
 {
     // create a track and scene info to work with change the constants above as required
     var trackBank = host.createMainTrackBank(NUM_TRACKS,0,NUM_SCENES);
+    var transport = host.createTransport();
+    transport.isPlaying().addValueObserver(function(playing) {
+        if(!playing) {
+            tracks = [];
+        }
+    });
     for(var i=0; i < NUM_TRACKS; ++i)
     {
         var track = trackBank.getTrack(i);
@@ -40,10 +46,14 @@ function init ()
         if(slotBank) {
             slotBank.addIsPlayingObserver(
                 // call the function wrapper above 
-                createTrackCallback(slotBank, i,
+                createTrackCallback(transport, slotBank, i,
                     // the function that gets executed when a clips playing state changes
-                    function(slotBank, trackIndex, index, playing)
+                    function(transport, slotBank, trackIndex, index, playing)
                     {
+                        if(!transport.isPlaying().get()) {
+                            tracks = [];
+                            return;
+                        }
                         // extend the array tracking playing state
                         if(!tracks[trackIndex]) {
                             tracks[trackIndex] = {}
